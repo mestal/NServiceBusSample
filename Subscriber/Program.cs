1b9e3a3ae0276520a583aceac5d35ebf23bfbab6
mestal <mestal@yahoo.com>
2017-08-12 17:23:39 +0300

    FirstInit

---
+++ Subscriber/Program.cs
@@ -0,0 +1,39 @@
+﻿using NServiceBus;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Subscriber
+{
+    class Program
+    {
+        static void Main()
+        {
+            AsyncMain().GetAwaiter().GetResult();
+        }
+
+        static async Task AsyncMain()
+        {
+            Console.Title = "Samples.StepByStep.Subscriber";
+            var endpointConfiguration = new EndpointConfiguration("Samples.StepByStep.Subscriber");
+            endpointConfiguration.UseSerialization<XmlSerializer>();
+            endpointConfiguration.UsePersistence<LearningPersistence>();
+            endpointConfiguration.UseTransport<LearningTransport>();
+
+            var endpointInstance = await Endpoint.Start(endpointConfiguration)
+                .ConfigureAwait(false);
+            try
+            {
+                Console.WriteLine("Press any key to exit");
+                Console.ReadKey();
+            }
+            finally
+            {
+                await endpointInstance.Stop()
+                    .ConfigureAwait(false);
+            }
+        }
+    }
+}
